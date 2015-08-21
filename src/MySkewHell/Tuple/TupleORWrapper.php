11cb46eda81f2c1b2e759134f867dffdf613d8f7
Beno!t POLASZEK <benoit@synapse-interactive.fr>
2015-08-21 15:42:34 +0200

    Removed override

--- src/MySkewHell/Tuple/TupleORWrapper.php
+++ src/MySkewHell/Tuple/TupleORWrapper.php
@@ -5,22 +5,6 @@ namespace MySkewHell\Tuple;
 class TupleORWrapper extends TupleConditionnalWrapper implements TupleInterface {
 
     /**
-     * Loads a mixed input to a TupleInterface object and adds it
-     * @param mixed $input
-     * @return $this
-     */
-    public function loadTuple($input) {
-        if (static::IsAnArrayOfTupleInterface($input))
-            return $this->setTuples($input);
-
-        elseif ($input instanceof TupleInterface)
-            return $this->addTuple($input);
-
-        elseif ((is_array($input) || is_string($input)) && TupleFactory::IsTuplable($input))
-            return $this->addTuple(TupleFactory::LoadTuple($input, false));
-    }
-
-    /**
      * String context
      * @return string
      */
